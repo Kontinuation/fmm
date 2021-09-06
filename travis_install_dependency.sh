969fbae1ddf01be1ecfac72962f89b29701856c0
Kristin Cowalcijk <morisama@yeah.net>
2021-09-06 13:54:05 +0800

    CMake will only build package for Python3; switched CI to use python3.

--- travis_install_dependency.sh
+++ travis_install_dependency.sh
@@ -7,5 +7,6 @@ else
   sudo add-apt-repository -y ppa:ubuntugis/ppa;
   sudo apt-get -q update;
   sudo apt-get -y install libboost-dev libboost-serialization-dev gdal-bin libgdal-dev make cmake;
-  sudo apt-get -y install swig python-dev;
+  sudo apt-get -y install swig python3-dev python3-pip;
 fi
+python3 -m pip install --upgrade pip setuptools wheel
